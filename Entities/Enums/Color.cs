4e0fb61b179b213fa6483f6b80b5b202c27b475e
Raphael Dos Santos <raphael_979@hotmail.com>
2019-10-20 20:49:54 -0300

    criado enum,classe do tipo enumerado com cores  Black,Blue,Red

---
+++ Entities/Enums/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figura.Entities.Enums
+{
+   enum Color //Do tipo enum,do tipo enumerado cor
+    {
+        Black,
+        Blue,
+        Red
+
+    }
+}
