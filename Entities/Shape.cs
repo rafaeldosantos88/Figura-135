68f3655b5e18b797335e91477ec81840d19fe211
Raphael Dos Santos <raphael_979@hotmail.com>
2019-10-20 20:41:34 -0300

    Criado classe Shape(figura)

---
+++ Entities/Shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figura.Entities
+{
+    class Shape                //Classe figura
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Figura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
