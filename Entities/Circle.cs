db29ceccdac6aaffb1339130903a7fbab28a6fa0
Raphael Dos Santos <raphael_979@hotmail.com>
2019-10-20 21:32:01 -0300

    Criada classe Circle ou circulo

---
+++ Entities/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Figura.Entities.Enums;
+
+namespace Figura.Entities
+{
+    class Circle :Shape
+    {
+        public double Radius{ get; set; }  //Raio do circulo
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
