ca8e45fdf526ebb8f709f18b46cddf45cd5fdaa3
Raphael Dos Santos <raphael_979@hotmail.com>
2019-10-20 21:21:54 -0300

    Criada classe Rectangle com Width largura,height altura com calculo da area

---
+++ Entities/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Figura.Entities.Enums;
+
+
+namespace Figura.Entities
+{
+    class Rectangle : Shape                            //vai herda da classe shape
+    {
+         public double  Width { get; set; }           //largura
+        public double Height { get; set; }           //Altura
+
+
+        public Rectangle(double width,double heigth,Color color) : base(color)
+        {
+            Width = width;
+           Height = heigth;
+
+        }
+        public override double Area()               //Calculando Area
+        {
+            return Width * Height;
+        }
+    }
+}
--- Entities/Shape.cs
+++ Entities/Shape.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using Figura.Entities.Enums;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Figura.Entities
 {
-    class Shape                //Classe figura
+    abstract class Shape                                //Classe figura,Eu fui obrigado colocar também que classe é abstrata por causa do metodo
     {
+        public Color  Color { get; set; }               //O tipo vai Color(ENUM) E O NOME Color também
 
+       
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();              //Porém a figura é de um tipo muito generico não tem como Eu calcular uma area baseado em um tipo tão generico assim.Então operação vai ser abstrata
     }
 }
